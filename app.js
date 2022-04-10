97401fc5fa268b2d3b8f15aad8e67258945f6f5e
mac <mac@MBP-de-mac.home>
2022-04-10 10:03:56 +0100

    added env vars\

--- app.js
+++ app.js
@@ -4,11 +4,11 @@ const bodyParser = require('body-parser')
 const cors = require('cors');
 
 const db = mysql.createConnection({
-  host        : 'localhost',
-  user        : 'root',
-  password    : 'root',
-  database    : 'wsa_transactions',
-  port        : '8889'
+  host        :  process.env.DB_HOST,
+  user        :  process.env.DB_USER,
+  password    :  process.env.DB_PASSWORD,
+  database    :  process.env.DB_DATABASE,
+  port        :  process.env.DB_PORT
 });
 
 const port = process.env.PORT || 3000
